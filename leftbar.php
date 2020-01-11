cffaab982bb3e9f34a55d3762c7a7d9d74bfa8ce
Jesbinks <59465500+Jesbinks@users.noreply.github.com>
2020-01-10 16:00:49 -1200

    created dashboard.php,leftbar.php,navbar.php

---
+++ leftbar.php
@@ -0,0 +1,37 @@
+ <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default"> 
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="index.html"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                    </li>
+                   <!--  <li class="menu-title">UI elements</li> /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>view feedback</a>
+                       <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">feedback</a></li>
+                            <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">guest feedback</a></li>
+                        </ul>
+                          
+                    </li>
+                      <li class="menu-item-has-children dropdown">
+                
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>remove student or faculty</a>
+                       <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">student table</a></li>
+                            <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">faculty table</a></li>
+                        </ul>
+                          
+                    </li>
+                   </li>
+                      <li class="menu-item-has-children dropdown">
+                
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>change moderator</a>
+                       <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">moderator</a></li>
+<!--                             <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">faculty table</a></li -->
+                        </ul>
+                          
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
